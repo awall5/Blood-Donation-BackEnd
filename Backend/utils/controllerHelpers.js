8ae5c1b2113dec023f52bdfa0132c2ee42f02531
awall5 <awalmanga25@gmail.com>
2025-07-17 20:02:39 +0530

    Refactor handleGenericAPIError for improved readability and consistency

--- Backend/utils/controllerHelpers.js
+++ Backend/utils/controllerHelpers.js
@@ -1,19 +1,21 @@
 const handleGenericAPIError = (funName, req, res, err) => {
-    console.log(`----- ❌ Error in ${funName} ---------`);
-    console.log(err.message);
-    console.log(`-----------------------------------`);
+  console.log(`----- ❌ Error in ${funName} ---------`);
+  console.log(err.message);
+  console.log(`-----------------------------------`);
 
-    if (err.name === "ValidationError" || err.code == "11000") {
-        res.status(400).json({ isSuccess: false, message: `Err: ${err.message}`, data: {} });
-    }
-
-    res.status(500).json({
-        isSuccess: false,
-        message: "Internal Server Error",
-        data: {
-            errorMessage: err.name,
-        },
+  if (err.name === "ValidationError" || err.code == "11000") {
+    return res.status(400).json({
+      isSuccess: false,
+      message: `Err: ${err.message}`,
+      data: {},
     });
-};
+  }
 
-module.exports = { handleGenericAPIError };
+  return res.status(500).json({
+    isSuccess: false,
+    message: "Internal Server Error",
+    data: {
+      errorMessage: err.name,
+    },
+  });
+};
